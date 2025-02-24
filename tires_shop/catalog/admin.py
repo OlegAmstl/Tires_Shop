b23eee9c9382d05bbabcf393d300f88a16ab9143
Oleg <amstl@mail.ru>
2025-02-24 17:10:22 +0700

    Регистрация моделей Brand, Tire в админ панели

--- tires_shop/catalog/admin.py
+++ tires_shop/catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Brand, Tire
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(Tire)
+class TireAdmin(admin.ModelAdmin):
+    list_display = ['name', 'brand', 'diameter', 'width', 'height', 'season', 'price', 'stock', 'image']
+    list_filter = ['brand', 'season']
+    search_fields = ['name', 'brand__name']
+    readonly_fields = ['image']
+
