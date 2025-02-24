05f9ebc2d5cd5cf04360dfc4a28fa30d682f950c
Oleg <amstl@mail.ru>
2025-02-24 16:51:08 +0700

    Написал модели Brand, Tire

--- tires_shop/catalog/models.py
+++ tires_shop/catalog/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    """
+    Бренд шин.
+    """
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tire(models.Model):
+    """
+    Модель шины
+
+    Attributes:
+        brand (models.ForeignKey): Ссылка на бренд шины.
+        name (models.CharField): Название модели шины.
+        diameter (models.DecimalField): Диаметр шины.
+        width (models.IntegerField): Ширина шины.
+        height (models.IntegerField): Высота шины.
+        season (models.CharField): Сезонность шины.
+        price (models.DecimalField): Цена шины.
+        stock (models.IntegerField): Остатки шины на складе.
+        image (models.ImageField): Изображение шины.
+
+    Methods:
+        __str__(): Возвращает строковое представление объекта в формате "Бренд Модель Диаметр Сезон".
+    """
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    diameter = models.DecimalField(max_digits=4, decimal_places=1)
+    width = models.IntegerField()
+    height = models.IntegerField()
+    season = models.CharField(
+        choices=[('summer', 'Летние'), ('winter', 'Зимние'),
+                 ('allseason', 'Всесезонные')], max_length=10)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()  # Остатки
+    image = models.ImageField(upload_to='tires/', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.brand.name} {self.name} {self.diameter} {self.season}"
