255eae337d84a6662a16e864553cd0053b32dd80
Oleg <amstl@mail.ru>
2025-02-24 16:58:18 +0700

    Русификация модели Brand, Tire

--- tires_shop/catalog/models.py
+++ tires_shop/catalog/models.py
@@ -5,7 +5,8 @@ class Brand(models.Model):
     """
     Бренд шин.
     """
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True,
+                            verbose_name = "Название бренда")
 
     def __str__(self):
         return self.name
@@ -30,16 +31,21 @@ class Tire(models.Model):
         __str__(): Возвращает строковое представление объекта в формате "Бренд Модель Диаметр Сезон".
     """
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    diameter = models.DecimalField(max_digits=4, decimal_places=1)
-    width = models.IntegerField()
-    height = models.IntegerField()
+    name = models.CharField(max_length=255, verbose_name="Наименование")
+    diameter = models.DecimalField(max_digits=4, decimal_places=1,
+                                   verbose_name="Диаметр")
+    width = models.IntegerField(verbose_name="Ширина")
+    height = models.IntegerField(verbose_name="Высота профиля")
     season = models.CharField(
         choices=[('summer', 'Летние'), ('winter', 'Зимние'),
-                 ('allseason', 'Всесезонные')], max_length=10)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField()  # Остатки
-    image = models.ImageField(upload_to='tires/', null=True, blank=True)
+                 ('allseason', 'Всесезонные')], max_length=10,
+        verbose_name = "Сезон")
+
+    price = models.DecimalField(max_digits=10, decimal_places=2,
+                                verbose_name = "Цена")
+    stock = models.IntegerField(verbose_name = "Остатки")  # Остатки
+    image = models.ImageField(upload_to='tires/', null=True, blank=True,
+                              verbose_name = "Изображение")
 
     def __str__(self):
         return f"{self.brand.name} {self.name} {self.diameter} {self.season}"
